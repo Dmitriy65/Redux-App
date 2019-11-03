e2cfeb0151273a7146cb8fc4f113a045a1b24110
Dmitriy65 <mr.dima19980@gmail.com>
2019-11-03 15:08:32 +0300

    added middlewars function and a small functionality

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,13 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export class Page extends React.Component {
+  onBtnClick = e => {
+    const year = +e.currentTarget.innerText
+    this.props.getPhotos(year)
+  }
   render() {
-    const { year, photos } = this.props
+    const { year, photos, isFetching } = this.props
     return (
-      <div>
+      <div className="ib page">
         <p>
-          У тебя {photos.length} фото за {year} год
+          <button className="btn" onClick={this.onBtnClick}>
+            2018
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2017
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2016
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2015
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2014
+          </button>
         </p>
+        <h3>{year} год</h3>
+        {isFetching ? <p>Загрузка...</p> : <p>У тебя {photos.length} фото.</p>}
       </div>
     )
   }
@@ -17,4 +37,6 @@ export class Page extends React.Component {
 Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
+  getPhotos: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,28 +2,35 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { User } from '../components/User'
 import { Page } from '../components/Page'
-import './App.css'
-
+import { getPhotos } from '../actions/pageActions'
 class App extends Component {
   render() {
-    const { user, page } = this.props
+    const { user, page, getPhotosAction } = this.props
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Мой топ фото</h1>
-        </header>
+      <div className="app">
+        <Page
+          photos={page.photos}
+          year={page.year}
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
         <User name={user.name} />
-        <Page photos={page.photos} year={page.year} />
       </div>
     )
   }
 }
-
 const mapStateToProps = store => {
   return {
     user: store.user,
     page: store.page,
   }
 }
-
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = dispatch => {
+  return {
+    getPhotosAction: year => dispatch(getPhotos(year)),
+  }
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
